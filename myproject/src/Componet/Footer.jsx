ca3ba3c5d735529854e798ce5c1fd24f1d741c02
tmanju01 <thakor.manju.391@gmail.com>
2023-06-16 10:22:24 +0530

    Create Footer.jsx

---
+++ myproject/src/Componet/Footer.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+function Footer(props) {
+    return (
+        <>
+           <div className="container text-center footer mt-5">
+                <nav className='text '>
+                    <a href='' className='mx-3'>Log In</a>
+                    <a href='' className='mx-3'>Newsletters</a>
+                    <a href='' className='mx-3'>About Us</a>
+                    <a href='' className='mx-3'>Blog</a>
+                    <a href='' className='mx-3'>Tools</a>
+                    <a href='' className='mx-3'>Publishers</a>
+                    <a href='' className='mx-3'>Help</a>
+                    <a href='' className='mx-3'>Terms</a>
+                    <a href='' className='mx-3'>Privacy Policy</a>
+                    <a href='' className='mx-3'>Sitemap</a>
+                    <p className='mt-5 mb-5'><a href='' className='copy'>&copy;2023 Flipboard</a></p>
+                    
+                </nav>
+            </div>  
+        </>
+    );
+}
+
+export default Footer;
