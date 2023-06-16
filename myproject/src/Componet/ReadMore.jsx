a105b4847d77444abd73eca73266888dc62f3df9
tmanju01 <thakor.manju.391@gmail.com>
2023-06-16 10:21:47 +0530

    Create ReadMore.jsx

---
+++ myproject/src/Componet/ReadMore.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function ReadMore(props) {
+    return (
+        <>
+            <div className="container text-center my-4">
+           
+
+           <button className='readmore'>Read More</button>
+      
+    </div> 
+        </>
+    );
+}
+
+export default ReadMore;
